0343638701b227d623430f717fe4882fb875dbbe
carth <me@carth.xyz>
2024-12-14 09:24:43 +0100

    update 1.33.6

--- docker-build-base.sh
+++ docker-build-base.sh
@@ -1,3 +1,3 @@
-docker buildx build --platform linux/amd64 -f Dockerfile-base . -t c4rth/backstage-base:20241018.2 -t c4rth/backtage-base:latest
+docker buildx build --platform linux/amd64 -f Dockerfile-base . -t c4rth/backstage-base:20241214.1 -t c4rth/backtage-base:latest
 
 # DOCKER_DEFAULT_PLATFORM="linux/amd64" docker build -f Dockerfile-base . -t c4rth/backstage-base:20240911.2 -t c4rth/backtage-base:latest
