b4e08a9e1db80087189ebafe7108882376a568b3
carth <me@carth.xyz>
2024-12-21 17:00:13 +0100

    docker

--- docker-build-base.sh
+++ docker-build-base.sh
@@ -1,3 +1,3 @@
-docker build -f Dockerfile-base . -t c4rth/backstage-base:20241215.1 -t c4rth/backtage-base:latest
+docker build -f Dockerfile-base . -t c4rth/backstage-base:20241221.1
 
 # DOCKER_DEFAULT_PLATFORM="linux/amd64" docker build -f Dockerfile-base . -t c4rth/backstage-base:20240911.2 -t c4rth/backtage-base:latest
